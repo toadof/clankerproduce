5aa507729b34f29b1488cf10b58ca286b496d098
toadof <gautampl444@gmail.com>
2025-11-13 19:40:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import analyzeRoutes from './routes/analyze.routes.js';
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const app = express();
 app.use(express.json());
